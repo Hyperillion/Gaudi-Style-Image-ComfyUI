fa9652f17e5b558a8812587d51fad50d27ec9202
HyperillionDesktop <andyyejr@gmail.com>
2024-06-07 03:07:28 +0800

    set chrome to kiosk mode

--- run.bat
+++ run.bat
@@ -3,5 +3,5 @@ start cmd /k "python ..\main.py --listen"
 timeout -nobreak 15
 start cmd /k "cd WebUI\ && node displayImageServer.js"
 start cmd /k "cd pythonModule && python python_comfy_api.py"
-start .\WebUI\Display.lnk
+start .\WebUI\Display.lnk --kiosk
 
