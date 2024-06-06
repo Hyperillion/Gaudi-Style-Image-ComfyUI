5872b44c6e506e1a390d2b2c511991c49a744179
HyperillionDesktop <andyyejr@gmail.com>
2024-06-06 22:51:59 +0800

    webui function finished

--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 start cmd /k "python ..\main.py --listen"
 timeout -nobreak 12
 start cmd /k "cd WebUI\ && python server.py"
-start http://127.0.0.1:8000/
+"C:\Program Files\Google\Chrome\Application\chrome.exe" "C:\Users\Public\Gaudi\ComfyUI\ComfyUI_windows_portable\ComfyUI\Gaudi-Style-Image-ComfyUI\WebUI\index.html"
 start cmd /k "cd pythonModule && python python_comfy_api.py"
