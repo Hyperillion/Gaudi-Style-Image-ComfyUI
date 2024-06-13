979b132c67e54d358e911762443b9a7961f92f13
Hyperillion <andyyejr@gmail.com>
2024-06-13 19:02:25 +0800

    final tunning check

--- run.bat
+++ run.bat
@@ -1,9 +1,9 @@
 @echo off
 @REM del /s/q WebUI\public\log\*.json
 start cmd /k "python ..\main.py --listen"
-timeout -nobreak 20
+timeout -nobreak 45
 start cmd /k "cd WebUI\ && node displayImageServer.js"
 start cmd /k "cd pythonModule && python python_comfy_api.py"
 start cmd /k "cd pythonModule && python tencentcloudCheckSDK.py"
+timeout -nobreak 10
 start .\WebUI\Display.lnk --kiosk
-
